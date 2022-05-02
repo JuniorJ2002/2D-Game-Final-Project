1ec146f44006c90870aeefdda11806387c62e0de
JuniorJ2002 <junior_julca@bloomfield.edu>
2022-05-02 17:42:32 -0400

    5/2/22 Part 1

---
+++ Project Runner Z/Assets/Scripts/SlimeSpawner.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI; // Unity AI
+public class SlimeSpawner : MonoBehaviour
+{
+    public GameObject[] objectPrefabs;
+    private float spawnDelay = 2;
+    private float spawnInterval = 8f;
+    public float chasingInterval = 2f;
+    public float chasingDistance = 12f;
+
+    private GameObject HeroKnight;
+    private float chasingTimer;
+
+  
+    private NavMeshAgent agent; // Unity AI
+
+    private PlayerController PlayerControllerScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObject", spawnDelay, spawnInterval);
+        PlayerControllerScript = GameObject.Find("HeroKnight").GetComponent<PlayerController>();
+    }
+
+    // Spawn obstacles
+    void SpawnObject()
+    {
+        // Set random spawn location and random object index
+        Vector2 spawnLocation = new Vector2(Random.Range(-10, 10), Random.Range(-4, -4));
+        int index = Random.Range(0, objectPrefabs.Length);
+
+        // If game is still active, spawn new object
+        if (!PlayerControllerScript.gameOver)
+        {
+            Instantiate(objectPrefabs[index], spawnLocation, objectPrefabs[index].transform.rotation);
+        }
+
+    }
+
+    private void Update()
+    {
+        // Chasing Logic.
+        chasingTimer -= Time.deltaTime;
+        if (chasingTimer <= 0 && Vector2.Distance(transform.position, HeroKnight.transform.position) <= chasingDistance)
+        {
+            chasingTimer = chasingInterval;
+            agent.SetDestination(HeroKnight.transform.position); // Unity AI
+        }
+    }
+
+
+}
--- Project Runner Z/Assets/Scripts/SpawnManager.cs
+++ Project Runner Z/Assets/Scripts/SpawnManager.cs
@@ -31,6 +31,8 @@ public class SpawnManager : MonoBehaviour
         }
 
     }
+
+  
 }
 
 
