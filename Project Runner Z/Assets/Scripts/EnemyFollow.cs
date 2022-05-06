f21c8ac1bf7e55b7decb258b912ec7a3d4a1e4ee
emcupo <102879140+emcupo@users.noreply.github.com>
2022-05-06 12:02:51 -0400

    enemy following

---
+++ Project Runner Z/Assets/Scripts/EnemyFollow.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollow : MonoBehaviour
+{
+
+    private Transform targetPlayer;
+    public float speed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetPlayer = GameObject.FindGameObjectWithTag("HeroKnight").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, targetPlayer.position, speed * Time.deltaTime);
+    }
+}
